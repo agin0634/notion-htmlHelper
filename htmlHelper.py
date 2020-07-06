b5d8e1600aedea95c161e72ad9f6419c4a008a86
Duri Shen <agin0511@gmail.com>
2020-07-06 18:47:10 +0800

    modify by template

--- htmlHelper.py
+++ htmlHelper.py
@@ -1,41 +1,48 @@
 from bs4 import BeautifulSoup
 import os
 import shutil
+import string
 
 base = os.path.dirname(os.path.abspath(__file__))
-t_html = '''
+'''
+t_html = 
 <link rel="stylesheet" href="https://agin0634.github.io/web/style.css">
 <link rel="stylesheet" type="text/css" href="https://agin0634.github.io/web/google-code-prettify/prettify.css">
 <script type="text/javascript" src="https://agin0634.github.io/web/google-code-prettify/prettify.js"></script>
+
 '''
 index = ""
 # modify html contents
 for dirPath, dirNames, fileNames in os.walk(base):
     for name in fileNames:
+        if name == "template.html":
+            t_path = os.path.join(base, dirPath, name)
+
+    for name in fileNames:
         if name.endswith(".html"):
             path = os.path.join(base, dirPath, name)
-            if not index and name != "index.html":
-                index = path
             soup = BeautifulSoup(open(path, "r", encoding='utf-8'), "html.parser")
-            t_soup = BeautifulSoup(t_html, "html.parser")
-            for x in soup.find_all('style'):
-                x.extract()
+            tmp_soup = BeautifulSoup(open(t_path, "r", encoding="utf-8"), "html.parser")
+            
+            div = tmp_soup.find('div', class_= "text-center")
+            try:
+                div.insert_before(soup.body)
+            except:
+                print("Fail")
 
-                head = soup.find('head')
-                head.append(t_soup)
+            # rename file name and copy new file
+            FN, tmp = "", ""
+            for s in name:
+                if s != " ":
+                    tmp += s.lower()
+                else:
+                    if len(tmp) <= 25:
+                        FN += tmp
+                    tmp = "-"
+            
+            dst = os.path.join(base,FN+".html")
 
-                body = soup.find('body')
-                body['onload'] = "PR.prettyPrint()"
+            html = tmp_soup.prettify("utf-8")
 
-                for y in soup.find_all('pre'):
-                    y['class']='prettyprint'
-
-            html = soup.contents
-            html = soup.prettify("utf-8")
-            with open(path, "wb") as file:
+            with open(dst, "wb") as file:
                 file.write(html)
-
-# create index.html
-src = index
-dst = os.path.join(base,"homepage.html")
-shutil.copy(src, dst)
